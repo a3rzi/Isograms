321eeba42e375f6bb3340b1d784013372be1e89f
a3rzi <amirhosseinqatar@gmail.com>
2024-02-05 19:21:52 +0330

    step1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,35 @@
 package org.example;
 
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //given
+        String str = "AmirhosSein";
+
+        //when
+        boolean result = isIsogram(str);
+        System.out.println(result);
+
+
     }
+
+    private static boolean isIsogram(String str) {
+        //Assume the empty string is an isogram
+        if (str.isEmpty()){
+            return true;
+        }
+        //Ignore letter case.
+        //convert to lower case
+        str = str.toLowerCase();
+        // Use a set to keep track of unique letters
+        // Iterate through each character in the string
+        // If the character is already in the set, it's a repeat
+        // Otherwise, add the character to the set
+        // If no repeating letters are found, it's an isogram
+        return true;
+
+    }
+
+
 }
---
+++ src/test/java/FunctionTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class FunctionTest {
+    @Test
+    void lowercase() {
+        String str = "HELLOworld";
+        System.out.println(str.toLowerCase());
+        System.out.println(str.toUpperCase());
+    }
+}
